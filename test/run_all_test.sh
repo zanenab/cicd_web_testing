c55e70913c50547742f23f30a2b010178f49ce66
zanenab <ezracota@gmail.com>
2024-10-27 14:09:45 -0500

    test workflow

--- test/run_all_test.sh
+++ test/run_all_test.sh
@@ -3,7 +3,6 @@ python -m http.server 8000
 
 python -m venv .venv
 source ./venv/bin/activate
-pip install robotframework
-pip install pip install robotframework-seleniumlibrary
+pip install -r requirements.txt
 
 robot -d test/results test/exists.robot
